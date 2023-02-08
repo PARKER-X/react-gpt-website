e8c358c9619286b365282456b86f5ee50bda4407
Harsh <harshrawat096@gmail.com>
2023-02-08 10:57:20 +0530

    final

--- src/components/feature/feature.jsx
+++ src/components/feature/feature.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import './style.css'
 
-const Feature = () => {
+const Features = ({title, text}) => {
     return (
         <>
-        Feature
+
+       
+            <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+  
         </>
     )
 }
 
-export default  Feature;
+export default  Features;
